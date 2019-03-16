816c651f7a709bef6b7b816ca83d65a624cc8b62
Raushan998 <raushan.raman23011999@gmail.com>
2019-03-16 20:03:03 +0530

    first commit

---
+++ Complex_object_c++.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+class Complex{
+public://class
+    double re,im;//Data members
+};
+int main(){
+//object n1 declared,initialised
+Complex n1={4.2,5.3};
+cout <<n1.re<<" "<<n1.im;//use
+return 0;
+}
